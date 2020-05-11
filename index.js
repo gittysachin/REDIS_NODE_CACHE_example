01410a16a20dd1e339a5b3d4d458fb7df6bf9db0
Sachin Jangid <34537025+gittysachin@users.noreply.github.com>
2020-05-11 13:41:44 +0530

    Redis Caching in Node.js

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const redis = require("redis");
+
+const PORT = process.env.PORT || 5000;
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+const client = redis.createClient(REDIS_PORT);
+// try running redis-cli then get gittysachin
+
+const app = express();
+
+// Set response 
+function setResponse(username, repos){
+    return `<h2>${username} has ${repos} Github repos.</h2>`;
+}
+
+// Make request to Github for data
+async function getRepos(req, res, next) {
+    try {
+        console.log('Fetching data...');
+
+        const { username } = req.params;
+        
+        const response = await fetch(`https://api.github.com/users/${username}`);
+
+        const data = await response.json();
+        
+        const repos = data.public_repos;
+
+        // Set data to Redis 
+        client.setex(username, 3600, repos);
+
+        res.send(setResponse(username, repos));
+    } catch (err) {
+        console.error(err);
+        res.status(500);
+    }
+}
+
+// Cache middleware 
+function cache(req, res, next) {
+    const { username } = req.params;
+    
+    client.get(username, (err, data) => {
+        if(err) throw err;
+
+        if(data !== null) {
+            res.send(setResponse(username, data));
+        } else {
+            next();
+        }
+    })
+}
+
+app.get("/repos/:username", cache, getRepos);
+// Go to the network tab in dev tool and refresh and check the finish time at bottom
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+});
